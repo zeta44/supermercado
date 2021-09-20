fee450c1f0c6f5255074f45908df0dc5200fa8ff
José Nigério Evangelista Lima Bezerra <nigerio@nimolabs.net>
2021-09-20 10:38:57 +0200

    ok

--- public/js/main.js
+++ public/js/main.js
@@ -10,22 +10,39 @@ $(document).ready(function () {
 
 
 //
-document.addEventListener("DOMContentLoaded", function(event) {
+document.addEventListener("DOMContentLoaded", function (event) {
 
 
     const cartButtons = document.querySelectorAll('.cart-button');
-    
+
     cartButtons.forEach(button => {
-    
-    button.addEventListener('click',cartClick);
-    
+
+        button.addEventListener('click', cartClick);
+
     });
-    
-    function cartClick(){
-    let button =this;
-    button.classList.add('clicked');
+
+    function cartClick() {
+        let button = this;
+        button.classList.add('clicked');
     }
-    
-    
-    
+
+
+
+});
+
+//
+// $(document).ready(function() {
+//     $("#adm").hide();
+// })
+
+$(document).ready(function(){
+    $("#adm").hide();
+    $("#adm_button").click(function(){
+      $("#adm").toggle(600);
     });
+  });
+
+
+// $("#adm_button").click(function () {
+//     $("#adm").toggle(1000);
+// });
